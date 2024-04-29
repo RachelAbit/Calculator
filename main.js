36c142a3c1d58754967382a6e679ba3b3e5ba156
Rachel_Abit <rachelannabit@gmail.com>
2024-04-29 09:26:47 +0800

    new changes

--- main.js
+++ main.js
@@ -6,15 +6,12 @@ let display_snum = '';//second number
 let display_ope = '';
 
 function displayVal(val){
-
     if(display_ope === ''){
-        display_fnum  += val; 
+       display_fnum += val;
     } else{
-        display_snum  += val;
+        display_snum  += val; //the second number that you will press is will be put in display_snum
     }
     output.textContent += val;
-    // display_fnum = output.textContent += val; 
-    // display_snum = output.textContent += val;
 }
 
 function operatorVal(val){
@@ -27,6 +24,7 @@ function clearAll(){
     display_ope = '';
     display_snum = '';
 }
+
 
 function operate(){
    let split = display_ope.split('');
@@ -43,8 +41,12 @@ function operate(){
       let multiply = fnum * snum;
       output.textContent = multiply;
    } else if(split.includes('/')){
-      let divide = fnum / snum;
-      output.textContent = divide;
-   } 
-   
+      let divide;
+      if(snum === 0){
+        output.textContent = 'ERROR';
+      } else {
+        divide = fnum / snum;
+        output.textContent = divide;
+      }     
+   }
 }
